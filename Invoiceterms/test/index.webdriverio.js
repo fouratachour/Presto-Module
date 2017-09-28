e146ad6cd6a8825faccffac74e2797277939a59d
fouratachour <fourat.ach@gmail.com>
2017-09-28 14:57:33 +0100

    Adding Invoice terms folder

---
+++ Invoiceterms/test/index.webdriverio.js
@@ -0,0 +1,37 @@
+'use strict';
+var should = require('should');
+var common = require('./common.webdriverio');
+
+
+describe('Allscenario', function(){
+	common.initMocha.call(this);
+	
+	before(function (done) {
+		this.client = common.getClient();
+		this.client.call(done);
+	});
+	
+	after(function (done) {
+		this.client
+			.end()
+			.call(done);
+	});
+
+    // Test case n°1 = Check the default configuration
+	 require('./scenario/FO/buy_product.webdriverio');
+	 require('./scenario/BO/default_configuration.webdriverio');
+
+
+    // Test case n°2 = Check the added configuration
+	  require('./scenario/FO/buy_product.webdriverio');
+	  require('./scenario/BO/invoiceterms_default.webdriverio');
+
+    // Test case n°3 = Modify terms and conditions of use
+      require('./scenario/FO/buy_product.webdriverio');
+	  require('./scenario/BO/edit_invoiceterms.webdriverio');
+
+      require('./scenario/FO/buy_product.webdriverio');
+	  require('./scenario/BO/check_edited_invoiceterms.webdriverio');
+
+
+});
