9464fb212c6631cf8a4ce614dbad8ab3cc1400c4
nesrine abdmouleh <nesrine.abdmouleh@sifast.com>
2017-10-05 11:26:49 +0100

    fix_index.webdriverio

--- SEOImage_module/test/index.webdriverio.js
+++ SEOImage_module/test/index.webdriverio.js
@@ -18,20 +18,20 @@ describe('Allscenario', function(){
 	});
 
     // Add EN rule in all categories in BO and check it in FO
-    /*require('./scenario/BO/add_EN_rule_all_categories.webdriverio');
-    require('./scenario/FO/check_EN_rule_all_categories.webdriverio');*/
+    require('./scenario/BO/add_EN_rule_all_categories.webdriverio');
+    require('./scenario/FO/check_EN_rule_all_categories.webdriverio');
 
     // Edit EN rule in all categories in BO and check it in FO
-	/*require('./scenario/BO/edit_EN_rule_all_categories.webdriverio');
-	require('./scenario/FO/check_edited_EN_rule_all_categories.webdriverio');*/
+	require('./scenario/BO/edit_EN_rule_all_categories.webdriverio');
+	require('./scenario/FO/check_edited_EN_rule_all_categories.webdriverio');
 
 	// Add EN rule in a specific category in BO and check it in FO
-	/*require('./scenario/BO/add_EN_rule_specific_category.webdriverio');
-	require('./scenario/FO/check_EN_rule_specific_category.webdriverio');*/
+	require('./scenario/BO/add_EN_rule_specific_category.webdriverio');
+	require('./scenario/FO/check_EN_rule_specific_category.webdriverio');
 
 	// Add FR rule in all categories and in specific category
-	/*require('./scenario/BO/add_FR_rule_all_categories.webdriverio');
-    require('./scenario/BO/add_FR_rule_specific_category.webdriverio');*/
+	require('./scenario/BO/add_FR_rule_all_categories.webdriverio');
+    require('./scenario/BO/add_FR_rule_specific_category.webdriverio');
 
 	//Check all rules in FO
     require('./scenario/FO/check_all_rules.webdriverio')
